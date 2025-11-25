a171b7d7c460882dd8f47fff93d2ac1c246e1f71
balasakthimaha <145747499+balasakthimaha@users.noreply.github.com>
2025-11-25 10:29:58 +0530

    Add files via upload

--- kmeans.py
+++ kmeans.py
@@ -7,36 +7,40 @@ def initialize_centroids(X, k):
     return X[idx]
 
 def assign_clusters(X, centroids):
-    dists = np.linalg.norm(X[:,None] - centroids[None,:], axis=2)
-    return np.argmin(dists, axis=1)
+    distances = np.linalg.norm(X[:, None] - centroids[None, :], axis=2)
+    return np.argmin(distances, axis=1)
 
 def update_centroids(X, labels, k):
-    return np.array([X[labels==i].mean(axis=0) for i in range(k)])
+    return np.array([X[labels == i].mean(axis=0) for i in range(k)])
 
-def kmeans(X, k, max_iters=100):
+def compute_inertia(X, centroids, labels):
+    return sum(np.linalg.norm(X[i] - centroids[labels[i]])**2 for i in range(len(X)))
+
+def kmeans(X, k, max_iters=100, tol=1e-4):
     centroids = initialize_centroids(X, k)
     for _ in range(max_iters):
         labels = assign_clusters(X, centroids)
         new_centroids = update_centroids(X, labels, k)
-        if np.allclose(centroids, new_centroids):
+        shift = np.linalg.norm(new_centroids - centroids)
+        if shift < tol:
             break
         centroids = new_centroids
-    inertia = np.sum((np.linalg.norm(X - centroids[labels], axis=1)**2))
+    inertia = compute_inertia(X, centroids, labels)
     return centroids, labels, inertia
 
 if __name__ == "__main__":
-    np.random.seed(42)
-    c1 = np.random.randn(100,2) + [0,0]
-    c2 = np.random.randn(100,2) + [5,5]
-    c3 = np.random.randn(100,2) + [0,5]
-    X = np.vstack([c1,c2,c3])
+    np.random.seed(0)
+    c1 = np.random.randn(100, 2) + np.array([0, 0])
+    c2 = np.random.randn(100, 2) + np.array([5, 5])
+    c3 = np.random.randn(100, 2) + np.array([10, 0])
+    X = np.vstack([c1, c2, c3])
 
-    inertias = {}
-    for k in range(2,7):
-        _,_, inertia = kmeans(X,k)
-        inertias[k] = inertia
-        print(f"K={k}, Inertia={inertia}")
+    k = 3
+    centroids, labels, inertia = kmeans(X, k)
 
-    print("\nElbow Method:")
-    for k,v in inertias.items():
-        print(f"{k}: {'*'*int(v/100)}")
+    print("Final Inertia:", inertia)
+
+    plt.scatter(X[:,0], X[:,1], c=labels)
+    plt.scatter(centroids[:,0], centroids[:,1], marker='X', s=200)
+    plt.title("K-Means Clustering Result")
+    plt.savefig("clusters.png")
